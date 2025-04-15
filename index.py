98cbe448622ac13a3ab53af098e95cc1060e2ed7
macilah-carter <museemacilah@gmail.com>
2025-04-15 10:36:59 +0300

    python classes

--- index.py
+++ index.py
@@ -30,13 +30,43 @@ class Subgroup(Superhero):
         return f"They belong to this group {self.group}"
 
 
-hero = Superhero("Flash", ["Speed", "Agility"], 9.5, 30, 101)
-print(hero)
 
-group_hero = Subgroup("Ironman", ["Intelligence", "Flight"], 9.8, 45, "Avengers", 102)
+
+# Activity 2: Polymorphism Challenge! 🎭
+
+
+class Vehicle:
+    def move(self):
+        return f"Movement for all"
+
+class Car:
+    def move(self):
+        return f"Driving"
+
+class Plane:
+    def move(self):
+        return f"Flying"
+        
+class Boat:
+    def move(self):
+        return f"Sailing"
+    
+class Bicycle:
+    def move(self):
+        return f"Cycling"
+
+
+
+car = Car()
+print(car.move)
+
+# hero = Superhero("Flash", ["Speed", "Agility"], 9.5, 30, 101)
+# print(hero)
+
+# group_hero = Subgroup("Ironman", ["Intelligence", "Flight"], 9.8, 45, "Avengers", 102)
 # print(group_hero)
 # print(group_hero.Group())
 
-print(hero.id)
-hero.id = 102
-print(hero.id)
+# print(hero.id)
+# hero.id = 102
+# print(hero.id)
